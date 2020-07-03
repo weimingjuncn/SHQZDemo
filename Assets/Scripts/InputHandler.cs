4aea7edf50cd52c1f762163058f655f2bb020aa5
mingjun <mingjun4503@163.com>
2020-07-04 00:10:31 +0800

    first commit

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace SHQZ
+{
+    
+    public class InputHandler : MonoBehaviour
+    {
+        public float horizonal;
+        public float vertical;
+        public float moveAmount;
+        public float mouseX;
+        public float mouseY;
+
+        PlayerControls inputActions;
+
+        Vector2 movementInput;
+        Vector2 cameraInput;
+
+        private void OnEnable()
+        {
+            if(inputActions == null)
+            {
+                inputActions = new PlayerControls();
+                inputActions.PlayerMovements.Movement.performed += inputActions
+                    => movementInput = inputActions.ReadValue<Vector2>();
+
+            }
+
+            inputActions.Enable();
+        }
+
+        private void OnDisable()
+        {
+            inputActions.Disable();
+        }
+
+    }
+}
---
+++ Assets/Scripts/PrintOnOff.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PrintOnOff : MonoBehaviour
+{
+    void OnDisable()
+    {
+        Debug.Log("PrintOnDisable: script was disabled");
+    }
+
+    void OnEnable()
+    {
+        Debug.Log("PrintOnEnable: script was enabled");
+    }
+
+    void Update()
+    {
+#if UNITY_EDITOR
+        Debug.Log("Editor causes this Update");
+#endif
+    }
+}
